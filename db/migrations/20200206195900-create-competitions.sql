ea9a3f4a46545f372d6020da401fc484ae65ae26
Ali Haidar <ahaid090@uottawa.ca>
2020-02-06 21:49:26 -0500

    finished seed

--- db/migrations/20200206195900-create-competitions.sql
+++ db/migrations/20200206195900-create-competitions.sql
@@ -1,14 +1,3 @@
-CREATE TABLE athletes (
-  id int,
-  identifier varchar(50),
-  created timestamp,
-  modified timestamp,
-  athleteName varchar(50),
-  dob date,
-  identified_gender varchar(6),
-  PRIMARY KEY (id)
-);
-
 ALTER TABLE athletes
 ADD competitionId int;
 
