0b47af735169d32f40325f83dae6de9aaee4c322
Ivor <ivor.benderavage@gmail.com>
2020-02-07 19:49:47 -0500

    Fixed table issue with application, also error handling

--- application/index.php
+++ application/index.php
@@ -43,35 +43,48 @@ Query here: <input type="text" name="query"><br>
 	if (isset($_POST['query'])) {
 
 	$sql = htmlspecialchars($_POST['query']);
-	$pdo = new PDO('pgsql:host=127.0.0.1;port=5432;dbname=TestDb;user=webuser');
-
-	$rows = $pdo->query($sql);
-
-	$firstrow = $rows->fetch();
-
-	echo '<table>';
-
-	echo "<tr>";	
-
-	foreach ($firstrow as $key => $value) {
-		echo "<th>$key</th>";
-	}
-
-	echo "</tr><tr>";
 	
-	foreach ($firstrow as $key => $value) {
-		echo "<td>$value</td>";
-	}
+	try { 
+        $pdo = new PDO('pgsql:host=127.0.0.1;port=5432;dbname=TestDb;user=webuser');
+        
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	echo "</tr>";
+        $rows = $pdo->query($sql);
 
-	while($row = $rows->fetch()) {
-		echo "<tr>";
-		foreach ($row as $key => $value) {
-			echo "<td>$value</td>";
-		}
-		echo "</tr>";
-	}
+        $firstrow = $rows->fetch();
+
+        echo '<table>';
+
+        echo "<tr>";	
+
+        foreach ($firstrow as $key => $value) {
+            if (is_string($key)) {
+                echo "<th>$key</th>";
+            }
+        }
+
+        echo "</tr><tr>";
+        
+        foreach ($firstrow as $key => $value) {
+            if (is_string($key)) {
+                echo "<td>$value</td>";
+            }
+        }
+
+        echo "</tr>";
+
+        while($row = $rows->fetch()) {
+            echo "<tr>";
+            foreach ($row as $key => $value) {
+                if (is_string($key)) {
+                    echo "<td>$value</td>";
+                }
+            }
+            echo "</tr>";
+    }
+} catch(PDOException $err) {
+    echo '<span style="color: #FF0000"><b>Invalid query!<br></b></span>'  . $err->getMessage();
+}
 ?>
 
 </table>
